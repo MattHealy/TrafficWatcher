e6664ba3b1b36a269b76a7c89563fb5896865059
Matt Healy <healmatt@gmail.com>
2017-05-11 20:03:34 +0800

    Changes for AWS Lambda

--- traffic.py
+++ traffic.py
@@ -2,46 +2,59 @@ import os
 import twitter
 import boto3
 
-HASHTAG = os.environ.get('TRAFFIC_HASHTAG')
-BUCKET = os.environ.get('TRAFFIC_BUCKET') 
-SCREEN_NAME = os.environ.get('TRAFFIC_HANDLE')
 
-CONSUMER_KEY = os.environ.get('TRAFFIC_CONSUMER_KEY')
-CONSUMER_SECRET = os.environ.get('TRAFFIC_CONSUMER_SECRET')
-ACCESS_TOKEN_KEY = os.environ.get('TRAFFIC_ACCESS_TOKEN_KEY')
-ACCESS_TOKEN_SECRET = os.environ.get('TRAFFIC_ACCESS_TOKEN_SECRET')
+def lambda_handler(event, context):
 
-api = twitter.Api(consumer_key=CONSUMER_KEY,
-                  consumer_secret=CONSUMER_SECRET,
-                  access_token_key=ACCESS_TOKEN_KEY,
-                  access_token_secret=ACCESS_TOKEN_SECRET)
+    # Twitter app settings
+    CONSUMER_KEY = os.environ.get('TRAFFIC_CONSUMER_KEY')
+    CONSUMER_SECRET = os.environ.get('TRAFFIC_CONSUMER_SECRET')
+    ACCESS_TOKEN_KEY = os.environ.get('TRAFFIC_ACCESS_TOKEN_KEY')
+    ACCESS_TOKEN_SECRET = os.environ.get('TRAFFIC_ACCESS_TOKEN_SECRET')
 
-client = boto3.client('s3')
+    HASHTAG = os.environ.get('TRAFFIC_HASHTAG')
+    BUCKET = os.environ.get('TRAFFIC_BUCKET')
+    SCREEN_NAME = os.environ.get('TRAFFIC_HANDLE')
 
-response = client.get_object(
-    Bucket=BUCKET,
-    Key='latest_tweet_id')
+    api = twitter.Api(consumer_key=CONSUMER_KEY,
+                      consumer_secret=CONSUMER_SECRET,
+                      access_token_key=ACCESS_TOKEN_KEY,
+                      access_token_secret=ACCESS_TOKEN_SECRET)
 
-previous_tweet = int(response.get('Body').read().decode())
+    client = boto3.client('s3')
 
-latest_tweet = previous_tweet
+    response = client.get_object(
+        Bucket=BUCKET,
+        Key='latest_tweet_id')
 
-print("Getting all tweets since {}".format(previous_tweet))
+    previous_tweet = int(response.get('Body').read().decode())
 
-statuses = api.GetUserTimeline(screen_name=SCREEN_NAME, trim_user=True,
-                               count=50, since_id=previous_tweet)
+    latest_tweet = previous_tweet
 
-for s in reversed(statuses):
+    print("Getting all tweets since {}".format(previous_tweet))
 
-    latest_tweet = s.id
+    statuses = api.GetUserTimeline(screen_name=SCREEN_NAME, trim_user=True,
+                                   count=50, since_id=previous_tweet)
 
-    if any([h.text == HASHTAG for h in s.hashtags]):
-        # Post to our twitter feed
-        print(s.id, s.text)
-        status = api.PostUpdate(s.text)
+    for s in reversed(statuses):
 
-# Record new marker in bucket
-if latest_tweet != previous_tweet:
-    print("Saving {} as latest tweet".format(latest_tweet))
-    response = client.put_object(Body=str(latest_tweet).encode(),
-                                 Bucket=BUCKET, Key='latest_tweet_id')
+        latest_tweet = s.id
+
+        if any([h.text == HASHTAG for h in s.hashtags]):
+            # Post to our twitter feed
+            print(s.id, s.text)
+            try:
+                status = api.PostUpdate(s.text)
+            except:
+                pass
+
+    # Record new marker in bucket
+    if latest_tweet != previous_tweet:
+        print("Saving {} as latest tweet".format(latest_tweet))
+        response = client.put_object(Body=str(latest_tweet).encode(),
+                                     Bucket=BUCKET, Key='latest_tweet_id')
+
+    return
+
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
